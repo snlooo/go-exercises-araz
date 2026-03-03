fac309d758795a727a0964f7fdb38b5bc2aa4840
Araz Mustafayayev <124189823+snlooo@users.noreply.github.com>
2026-03-03 16:02:49 +0400

    Return DivisionError directly instead of formatting
    Resolved

--- session-11/custerr/task1.go
+++ session-11/custerr/task1.go
@@ -20,7 +20,7 @@ var DivisionError = errors.New("Error: Division by zero is not allowed")
 func Divide(a, b float64) (float64, error) {
 
 	if b == 0 {
-		return 0, fmt.Errorf(DivisionError.Error())
+		return 0, DivisionError
 	}
 
 	return a / b, nil
